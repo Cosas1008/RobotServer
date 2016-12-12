c2600dd218713c457cf2781f71d553379b41fa0f
Cosas1008 <yuweiichen@gmail.com>
2016-12-12 19:06:36 +0800

    Dec12 push

--- src/DatagramServer.java
+++ src/DatagramServer.java
@@ -1,4 +1,7 @@
-import java.net.* ;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
 
 /**
  *  A simple datagram server
@@ -10,10 +13,24 @@ import java.net.* ;
 
 public class DatagramServer
 {
-   private final static int PACKETSIZE = 1024 ;
+   private final static int PACKETSIZE = 4096 ;
+//ip address RFVLSI is 192.168.1.134
+	private static String[] respo = new String[] { "59", "45", "52", "43", "20", "00", "68", "00", "01", "01", "00",
+			"00", "00", "00", "00", "00", "39", "39", "39", "39", "39", "39", "39", "39", "8a", "00", "01", "00", "01",
+			"02", "00", "00", "01", "00", "00", "00", "00", "00", "00", "00", "01", "00", "00", "00", "e8", "03", "00",
+			"00", "11", "00", "00", "00", "fc", "c7", "0e", "00", "7c", "ce", "00", "00", "6b", "19", "05", "00", "ff",
+			"0d", "ed", "ff", "d4", "45", "f2", "ff", "b4", "7a", "f7", "ff", "00", "00", "00", "00", "00", "00", "00",
+			"00", "00", "00", "00", "00", "01", "00", "00", "00", "03", "00", "00", "00", "00", "00", "00", "00", "00",
+			"00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00",
+			"00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00" };
+	private static String[] readRespo = new String[] { "59", "45", "52", "43", "20", "00", "34", "00", "03", "01", "01",
+			"00", "00", "00", "00", "80", "39", "39", "39", "39", "39", "39", "39", "39", "81", "00", "00", "00", "00",
+			"00", "00", "00", "10", "00", "00", "00", "00", "00", "00", "00", "01", "00", "00", "00", "00", "00", "00",
+			"00", "00", "00", "00", "00", "fc", "c7", "0e", "00", "7c", "ce", "00", "00", "6b", "19", "05", "00", "ff",
+			"0d", "ed", "ff", "d4", "45", "f2", "ff", "b4", "7a", "f7", "ff", "00", "00", "00", "00", "00", "00", "00",
+			"00" };
 
-   private static byte[] respo = new byte[]{22,23,24,25,26,27,28,29};
-   public static void main( String args[] )
+	public static void main(String args[])
    {
       // Check the arguments
       if( args.length != 1 )
@@ -26,13 +43,15 @@ public class DatagramServer
       {
          // Convert the argument to ensure that is it valid
          int port = Integer.parseInt( args[0] ) ;
-
+         byte[] data = hexStringToByteArray(readRespo);
          // Construct the socket
          DatagramSocket socket = new DatagramSocket( port ) ;
-
+         System.out.println("Data sent is : ");
+         for(byte i : data){
+        	 System.out.print(" " + i); 
+         }
          System.out.println( "The server is ready..." ) ;
-
-
+         
          for( ;; )
          {
             // Create a packet
@@ -44,15 +63,17 @@ public class DatagramServer
             // Print the packet
             System.out.println( packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()) ) ;
 
+            
             // Return the packet to the sender
             
             //retrive packet's information to send back response
             InetAddress clientIP = packet.getAddress();
             int clientPort = packet.getPort();
-            DatagramPacket response = new DatagramPacket(respo,respo.length,clientIP,clientPort );
+            DatagramPacket response = new DatagramPacket(data,data.length,clientIP,clientPort );
             socket.send( response ) ;
             System.out.println("Send to client IP : port are "+ clientIP.toString() +" @"+ clientPort);
-            System.out.println("Send response is : " + response.toString());
+            System.out.println("Send response length is : " + data.length);
+            
         }  
      }
      catch( Exception e )
@@ -60,4 +81,25 @@ public class DatagramServer
         System.out.println( e ) ;
      }
   }
+	public static byte[] hexStringToByteArray(String[] s) {
+		int length = s.length;
+		byte[] data = new byte[PACKETSIZE];
+		for (int j = 0; j < length; j++) {
+			for (int i = 0; i <= 2; i += 2) {
+				data[j] =  (byte) ((Character.digit(respo[j].charAt(0), 16) << 4)+ (Character.digit(respo[j].charAt(0 + 1), 16)));
+			}
+		}
+		for (int i = 33; i < data.length; i++) {
+			if ((i + 1) % 4 == 0 && i != 0) {
+				byte[] first = { data[i - 3], data[i - 2] };
+				byte[] second = { data[i - 1], data[i] };
+				data[i - 3] = second[1];
+				data[i - 2] = second[0];
+				data[i - 1] = first[1];
+				data[i] = first[0];
+				//System.out.println("OBYTES" + i + " : " + data[i - 3] + " " + data[i - 2] + " " + data[i - 1] + " " + data[i]);
+			}
+		}
+		return data;
+	    }
 }
